2798441ee6091301410b0eb15bc4db583b8fbe5b
akhil-06 <svam1008@gmail.com>
2024-12-26 21:57:33 +0530

    loops and conditionals

---
+++ day3_typeConversion_ControlStatements/index.js
@@ -0,0 +1,116 @@
+//functions
+//function declaration
+function sum(x, y){
+    // console.log(x+y);
+    return x+y;
+}
+//calling of a function
+let ans = sum(4,5);
+console.log(ans);
+
+
+
+
+
+
+
+
+
+//Loops
+//do-while
+// let i = 1;
+// do{
+//     console.log(i*20);
+//     i++;
+// }while(i<=10);
+
+//while loop
+// let num = 20;
+// let i =1;
+// while(i<=10){
+//     console.log(num*i);
+//     i++;
+// }
+
+//for loop
+// for(let i=1;i<=10;i++){
+//     console.log(num*i);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+//switch
+// let shippingOption = "Two-Day";
+// switch (shippingOption) {
+//     case "Standard":
+//         console.log("your order will be shipped in 5-7 days");
+//         break;
+//     case "Express":
+//         console.log("your order will be shipped in 2-3 days");
+//         break; 
+//     case "One-Day":
+//         console.log("your order will be shipped in 1 day");
+//         break;
+//     default:
+//         console.log("Invalid shipping option");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let spendAmount = 700;
+// if(spendAmount > 1000){
+//     let discount = 0.10 * spendAmount;
+//     console.log('You got a discount of 10% => ' + discount);
+// }else if(spendAmount > 500){
+//     let discount = 0.05 * spendAmount;
+//     console.log('You got a discount of 5% => ' + discount);
+// }else{
+//     console.log('No discount available');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// // console.log(5 === '5');
+// // console.log(typeof (35 + " Akhil" + " Sharma"));
+// let v1 = false;
+// let v2 = "";
+// // console.log(v1 == v2);
+
+// //Explicit type conversion
+// let number = "number";
+// let boolNumber = Number(number);
+// console.log(typeof boolNumber);
+
+
+
+
+
+
