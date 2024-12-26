406d985cad293cb6245cd5fdf67ac0e5da950615
akhil-06 <svam1008@gmail.com>
2024-12-26 20:18:05 +0530

    day2:- variables and data types

---
+++ day2_variables&DT/intro.js
@@ -0,0 +1,65 @@
+let a = 10;
+let b = 30;
+let c = 10;
+a **=2;
+// console.log(--a + ++b + c-- + b + --c +--a + --b + c + --c + --a + --b + c + --c + --a + --b + c);
+console.log(a);
+
+
+
+
+
+
+
+
+
+
+// let a = 78235482385285234;
+// let b = true;
+// let c = "Akhil";
+// let d = null;
+// let e = undefined;
+// let f = BigInt(782354823854877);
+// let g = Symbol("ObjectKey");
+// // console.log( a,typeof b,typeof c,typeof d,typeof e, f,typeof g);
+
+// let obj = {
+//     name:"Akhil",
+//     age:25,
+//     city:"Bangalore",
+//     isStudying:false
+// }
+// console.log(5**3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const a = 10;
+// const a = 20;
+
+// console.log(a);
+
+
+
+
+
+
+
+
+// console.log("Akhil sharma is running")
+// console.log("Akhil sharma is running")
+// console.log("Akhil sharma is running")
+// console.log("Akhil sharma is running")
+// console.log("Akhil sharma is running")
+// console.log("Akhil sharma is running")
+// console.log("Akhil sharma is running")
+
