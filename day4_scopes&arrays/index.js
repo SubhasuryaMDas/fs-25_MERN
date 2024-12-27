ecb35e8aeda0eb58894ed7e2012d2ecedfd543c4
akhil-06 <svam1008@gmail.com>
2024-12-27 21:55:15 +0530

    arrays half and scoping done

---
+++ day4_scopes&arrays/index.js
@@ -0,0 +1,61 @@
+let arr = [
+    {name:"test", score:100},
+    {name:"test2", score:200},
+    {name:"test3", score:300},
+    {name:"test4", score:250},
+    {name:"test5", score:150}
+]
+//splice method
+// Array.splice(startIndex, deletionCount, item1,2,3,4,,,,,)
+arr.splice(1,3)
+
+
+let newSlicedArray = arr.slice(1,3)
+// arr.shift()
+// arr.unshift({name:"test 10", score:1000}, {name:"test 11", score:200})
+// let deleted = arr.pop();
+// console.log(deleted)
+// console.log(arr.length);
+// arr.push({name:"test6", score:400}, true)
+// console.log(arr.reverse());
+console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = 10;
+// function outer(){
+//     let x = 10;
+//     x++;
+//     function inner(){
+//         let y = 20;
+//         --y;
+//         ++x;
+//         function innerMost(){
+//             x++;
+//             ++y;
+//             let z = 30;
+//             ++z
+//             console.log(x,y,z);
+//         }
+//         innerMost();
+//     }
+//     inner();
+// }
+// outer();
+
