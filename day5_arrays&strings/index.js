811a739c2a0902c198759c607cc17ecd6d72e323
akhil-06 <svam1008@gmail.com>
2024-12-30 21:58:51 +0530

    arrays and strings intro

---
+++ day5_arrays&strings/index.js
@@ -0,0 +1,70 @@
+let s1 = 'Akhil Sharma';
+let s2 = "Akhil Sharma";
+let s3 = `Akhil Sharma`;
+console.log(s1, s2, s3);
+
+let s4 = "She patiently waited for his number to be called.\nShe had no desire to be there, but her mom had insisted that she goo\nShe's resisted at first, but over time she realized it was simply easier to appease her and go\nMom tended to be that way. She would keep insisting until you \n wore down and did what she wanted So, here she sat, patiently waiting for her number to be called."
+console.log(s4);
+
+
+
+
+
+
+// let playerInfo = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+// delete playerInfo[10];
+// playerInfo[10] = "player";
+// console.log(playerInfo);
+
+
+
+
+
+
+
+
+
+
+// let playerInfoString = "Anuj - Priyanshu - Suraj"
+// let palyerSplit = playerInfoString.split(" - ")
+// // let playerJoin = playerInfo.join(" * ")
+// // let plyerToString = playerInfo.toString()
+// console.log(playerJoin);
+
+
+
+
+
+
+
+
+// let arr = [
+//     {name:"Ashish", score:"1000"},
+//     {name:"Mohit", score:"200"},
+//     {name:"Mansi", score:"300"},
+//     {name:"Abhishek", score:"250"},
+//     {name:"Yash", score:"150"}
+// ]
+
+// arr.sort(function(a,b){
+    // 1st way
+    // return b.name.localeCompare(a.name);
+
+    // 2nd way
+    // if(a.name < b.name) return -1;
+    // if(a.name > b.name) return 1;
+    // return 0
+
+// });
+// console.log(arr);
+
+
+//sort method
+// console.log(arr.sort());
+// let num = [1000, 200, 300, 500, 150];
+// console.log(num.sort());
+
+// console.log(num.sort(function(a,b){
+//     return b-a;
+// }));
+
