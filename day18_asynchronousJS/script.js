ba4dbe2d5b36daf66591ca28a19befe9c8012900
akhil-06 <svam1008@gmail.com>
2025-01-20 21:57:04 +0530

    asynchronous and event loop

---
+++ day18_asynchronousJS/script.js
@@ -0,0 +1,60 @@
+// event loop
+function printMe(){
+    console.log('print me!!');
+}
+
+function test(){
+    console.log('test');
+}
+
+
+setTimeout(printMe, 0);
+// test();
+// test();
+// test();test();test();test();test();test();test();test();
+// test();test();test();test();test();test();test();test();test();
+// test();
+// test();test();test();test();test();test();test();test();test();test();test();test();
+
+
+
+
+
+// timing functions
+// console.log("Starting");
+
+// setTimeout(()=>{
+//     console.log("fn1 is done");
+// }, 2000);
+
+// console.log("Ending");
+
+
+
+
+// let i=6000;
+// let id = setInterval(()=>{
+//     // clearInterval(id);
+//     console.log("fn2 is done", i--);
+// }, 1000)
+
+
+// console.log("Starting");
+// function fetching(){
+//     console.log("fn1 is started...");
+//     for(let i=0;i<10000000000;i++){
+
+//     }
+//     console.log("fn1 ended ...");
+// }
+
+// function showPoats(){
+//     console.log("fn2 is started...");
+//     for(let i=0;i<10000000000;i++){
+
+//     }
+//     console.log("fn2 ended ...");
+// }
+// fetching();
+// showPoats();
+// console.log("Ending");
