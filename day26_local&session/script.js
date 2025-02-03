f71e80c663aa7e103acf35f9b89cfedacd88688e
akhil-06 <svam1008@gmail.com>
2025-02-03 21:52:00 +0530

    localstroage and signup login

---
+++ day26_local&session/script.js
@@ -0,0 +1,19 @@
+
+
+
+// let arr = [1,2,3,4,5];
+// let arrStringified = JSON.stringify(arr);
+// localStorage.setItem("arr",arrStringified);
+// let value = JSON.parse(localStorage.getItem("arr"));
+// // let value = localStorage.getItem("arr");
+// console.log(value);
+
+
+
+// localStorage.setItem("name","Akhil");
+// localStorage.setItem("marks",100);
+
+// let name = localStorage.getItem("name");
+// // localStorage.removeItem("name");
+// localStorage.clear();
+// console.log(name);
